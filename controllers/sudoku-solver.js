17c881868a1a0c22fb292b9a6289edd14af7d413
zm412 <zm412412@gmail.com>
2021-02-19 19:19:50 -0500

    still working with region, but it already gives array

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,18 +1,101 @@
 class SudokuSolver {
-
-  validate(puzzleString) {
+  constructor(puzzleString, row, column, value){
+    this.puzzleString = puzzleString;
+    this.arrPuzzle = String( puzzleString ).split('')
+    this.row = row;
+    this.column = column;
+    this.value = value;
+    this.objRows = {
+      'A':{ nn:1, pos: [0, 8], rowRegion: 1 } , 
+      'B': { nn: 2, pos: [9, 17] , rowRegion: 1 }, 
+      'C': { nn: 3, pos: [18, 26], rowRegion: 1  }, 
+      'D': { nn: 4, pos: [27, 35] , rowRegion: 2 }, 
+      'E': { nn: 5, pos: [36, 44] , rowRegion: 2 }, 
+      'F': { nn: 6, pos: [45, 53] , rowRegion: 2 }, 
+      'G': { nn: 7, pos : [54, 62] , rowRegion: 3 }, 
+      'H': { nn: 8, pos : [63, 71] , rowRegion: 3 }, 
+      'I': { nn: 9, pos: [72, 80] , rowRegion: 3 }
+    };
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
-
+  validate() {
+    
   }
 
-  checkColPlacement(puzzleString, row, column, value) {
-
+  checkRowPlacement() {
+    let flag = true;
+      for(let i = this.objRows[this.row]['pos'][0]; i <= this.objRows[this.row]['pos'][1]; i++){
+        if(this.puzzleString[i] == this.value){
+            flag = false;
+          break
+        }
+    }
+    return flag
   }
 
-  checkRegionPlacement(puzzleString, row, column, value) {
+  checkColPlacement() {
+    let flag = true;
 
+    for(let j = this.column - 1; j < this.puzzleString.length; j += 9){
+        if(this.puzzleString[j] == this.value){
+            flag = false;
+          break
+        }
+      }
+    return flag
+  }
+
+  checkRegionPlacement() {
+    let region = [];
+    let temArrJ = [];
+
+    for(let j = 0, i = 0; j < this.puzzleString.length; j+=27, i++){
+      temArrJ[i] = [];
+      for(let k = j; k < j+ 27; k++){
+        temArrJ[i].push(this.puzzleString[k])
+      }
+      
+    }
+
+    let tempArrP = []
+    for(let s = 0; s < temArrJ.length; s++){
+      tempArrP[s] = []
+      for(let c = 0, temp=0; c < temArrJ[s].length; c+=3, temp++){
+         tempArrP[s][temp] = [] 
+        for(let p = c; p < c + 3; p++){
+         tempArrP[s][temp].push(temArrJ[s][p]) 
+        }
+      }
+    }
+
+    for(let i = 0; i < tempArrP.length; i++){
+      region[i] = [[],[],[]];
+      for(let m = 0; m < tempArrP[i].length; m++){
+
+        if(m % 3 == 0){
+          region[i][0].push(tempArrP[i][m])
+        }else if(m % 3 == 1){
+          region[i][1].push(tempArrP[i][m])
+        }else if(m % 3 == 2){
+          region[i][2].push(tempArrP[i][m])
+        }    }
+
+      }
+
+  
+for(let i = 0; i < region.length; i++){
+  for(let j = 0; j < region[i].length; j++){
+    region[i][j] = region[i][j].reduce((total, amount) => {
+  return total.concat(amount);
+}, []);
+  }
+}
+
+    region = region.reduce((total, amount) => {
+  return total.concat(amount);
+}, []);
+    console.log(region, 'region')
+      return region
   }
 
   solve(puzzleString) {
--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,25 @@
 'use strict';
 
 const SudokuSolver = require('../controllers/sudoku-solver.js');
+let solver = new SudokuSolver();
 
 module.exports = function (app) {
   
-  let solver = new SudokuSolver();
 
   app.route('/api/check')
     .post((req, res) => {
-
+      let puzzleString = req.body.puzzle;
+      let regex = req.body.coordinate.match(/(^[A-I])([1-9])$/)
+      let solver = new SudokuSolver(puzzleString, regex[1], regex[2], req.body.value);
+      //console.log(req.body, 'req.body')
+      //console.log(regex, 'regex')
+      //console.log(regex[1], '1')
+      let fl = solver.checkRowPlacement();
+      let col = solver.checkColPlacement();
+      let region = solver.checkRegionPlacement()
+      console.log(fl, 'fl')
+      console.log(col, 'col')
+      console.log(region, 'reg')
     });
     
   app.route('/api/solve')
