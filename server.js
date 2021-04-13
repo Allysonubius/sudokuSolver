eac149934d6b5d85b88100e58cdd1e5a8df823b5
allyson <allyson.brandao@venhapranuvem.com.br>
2021-04-13 10:00:25 -0300

    Update .env

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(function(req, res, next) {
 });
 
 //Start our server and tests!
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT;
 app.listen(PORT, function () {
   console.log("Listening on port " + PORT);
   // process.env.NODE_ENV='test'
